7b538d82b0a8725330d458f00a1c78eb5b1b50ab
YunSang Lee <qlqhwb@gmail.com>
2019-06-28 18:57:43 +0900

    Add linstener in DeskFileUpload

--- System/Secretary/DeskFileUpload.js
+++ System/Secretary/DeskFileUpload.js
@@ -1,15 +1,25 @@
 class DeskFileUpload extends DeskFile {
 	constructor(id, name) {
 		super(id, name, 'UPD', null);
-		this._progress = 0;
+		this._progress = 0.0;
 		this.size = 0;
+		this.listeners = [];
+	}
+
+	listen(callback) {
+		this.listeners.push(callback);
 	}
 
 	didFinishUpload() {
+		this.progress = 1.0;
+		this.listeners = [];
 	}
 
 	set progress(newValue) {
 		this._progress = newValue;
+		for(let callback in this.listeners) {
+			callback(this.progress);
+		}
 	}
 
 	get progress() {
